fc79f0a6e58313e387eb99179f9e53ebe22075d6
luissanvil2 <luis.sanvil.2@educa.jcyl.es>
2026-04-09 21:10:24 +0000

    Migración a Kubernetes con autoescalado completada

--- start.sh
+++ start.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-echo "Arrancando la aplicación Docker..."
-docker compose up --build
+set -e
+
+echo "1. Preparando entorno de Minikube (Métricas)..."
+minikube addons enable metrics-server
+
+echo "2. Construyendo imagen de la API en Minikube..."
+eval $(minikube docker-env)
+docker build -t mi-api-tareas:1.0 ./app
+
+echo "3. Creando el Secret para MongoDB..."
+kubectl delete secret db-secret --ignore-not-found
+kubectl create secret generic db-secret --from-literal=mongo-uri='mongodb://db:27017/tareas'
+
+echo "4. Desplegando en Kubernetes..."
+kubectl apply -f k8s/
+
+echo "5. Esperando a que los Pods estén listos..."
+kubectl wait --for=condition=ready pod -l app=api --timeout=90s
+
+echo "✅ ¡Aplicación desplegada correctamente en K8s!"
